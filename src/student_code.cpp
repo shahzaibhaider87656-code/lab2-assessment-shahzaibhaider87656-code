b83b87c689df7535d85947a275e5f49d8e8a713a
shahzaibhaider87656-code <shahzaibhaider87656@gmail.com>
2026-02-18 14:38:21 +0500

    Update student_code.cpp

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -10,6 +10,7 @@ int getIntValue() {
 float getFloatValue() {
     // Declare a variable var_float of type float and set its value to 10.75.
     //return var_float;
+    //shahzaib;
 }
 
 char getCharValue() {
